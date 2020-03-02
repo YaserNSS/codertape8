ddfe54050f2acb06a201956c9e62458b033a1fec
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-03-02 14:38:01 +0300

    Lec 3 Views

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('contact', function () {
+//     return view('contact');
+// });
+
+// Route::get('about', function () {
+//     return view('about');
+// });
+
+Route::view('contact', 'contact');
+Route::view('about', 'about');
