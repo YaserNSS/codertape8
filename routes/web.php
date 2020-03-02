e3d14b80b79975912331c70666142321be45ae9c
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-03-02 15:15:56 +0300

    Lec 4 Passing Data to Views

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,12 @@ Route::get('/', function () {
 
 Route::view('contact', 'contact');
 Route::view('about', 'about');
+
+Route::get('customers', function () {
+    $customers = [
+        'John Doe',
+        'Jane Doe',
+        'Bob The Builder'
+    ];
+    return view('internals.customers',compact('customers'));
+});
