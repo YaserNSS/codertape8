7e6ef15966d23429ab49ea4e3704118091e7d22f
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-03-03 13:34:59 +0300

    Lec 10 Add Email For Customers

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -4,13 +4,22 @@
 @section('content')
 <h1>Customers</h1>
 
-<form action="customers" class="pb-5" method="POST">
-    <div class="input-group">
-        <input type="text" name="name" id="">
-        {{$errors->first('name')}}
-    </div>
-    <button type="submit">Add Customer</button>
-    @csrf
+<form action="customers" method="POST" class="pb-5">
+
+        <p>Name :</p>
+        <div class="input-group ob-2">
+            <input type="text" name="name" value="{{old('name')}}">
+        </div>
+        <div>{{$errors->first('name')}}</div>
+
+        <p>Email :</p>
+        <div class="input-group">
+                <input type="email" name="email" value="{{old('email')}}">
+        </div>
+        <div>{{$errors->first('email')}}</div>
+
+        <button type="submit" class="btn btn-primary">Add Customer</button>
+        @csrf
 </form>
 
 
@@ -18,7 +27,7 @@
 
 <ul>
     @foreach ($customers as $customer)
-        <li>{{$customer->name}}</li>
+        <li>{{$customer->name}} <span class="text-muted">({{$customer->email}})</span></li>
     @endforeach
 </ul>
 @endsection
