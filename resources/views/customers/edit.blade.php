d31456945bdb8c3d3c155a770216357a9b0dc36e
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-03-04 16:47:48 +0300

    Lec 17 RESTFUL Controller P3

---
+++ resources/views/customers/edit.blade.php
@@ -0,0 +1,27 @@
+
+@extends('layout')
+
+@section('title','Edit Details For' . $customer->name)
+
+
+@section('content')
+
+<div class="row">
+    <div class="col-12">
+        <h1>Edit Details For {{$customer->name}}</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12">
+            <form action="/Codetaper/customers/{{$customer->id}}" method="POST">
+                @method('PATCH')
+                @include('customers.form')
+                <button type="submit" class="btn btn-primary">Save Customer</button>
+
+            </form>
+    </div>
+</div>
+
+@endsection
+
